bbbaec6ae224cbbf7b877df9672baa673fd536ab
Amir-Mirkazemi <Amir-Mirkazemi@users.noreply.github.com>
2026-06-28 14:36:36 -0700

    Remove footer location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -333,7 +333,6 @@ export default function Home() {
       </section>
 
       <footer className="footer">
-        <p>Las Vegas, NV</p>
         <a href="#top">Back to top</a>
       </footer>
     </main>
